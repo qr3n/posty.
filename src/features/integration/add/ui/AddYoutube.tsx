8dddd1a7c91433a68cacd3dce04da8dd9d79c328
qr3n <qren.freelance@gmail.com>
2024-04-08 02:34:54 -0700

    AddYoutube.tsx

--- src/features/integration/add/ui/AddYoutube.tsx
+++ src/features/integration/add/ui/AddYoutube.tsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { integrationModel } from "@entities/integration";
-import { Button } from "@shared/shadcn/ui/button";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { DropdownMenuItem } from "@shared/shadcn/ui/dropdown-menu";
 import Image from "next/image";
 import youtube from "./youtube.png";
 
 export const AddYoutube = () => {
-  const session = useUser()
-
   return (
     <div className='flex gap-1'>
       <Image src={youtube} alt={'youyube'} width={32} height={8}/>
