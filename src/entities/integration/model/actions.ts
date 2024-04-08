7df38baa3bfad7ad2dec25aafba322e5ce08eace
qren <qtter85@gmail.com>
2024-04-09 01:30:53 +0700

    Вщьфшт

--- src/entities/integration/model/actions.ts
+++ src/entities/integration/model/actions.ts
@@ -1,5 +1,5 @@
 import { TAllowedIntegrations } from "./types";
 
 export const addIntegration = (integration: TAllowedIntegrations, id: string) => {
-  window.location.href = `http://localhost:8000/integrations/${integration}/auth?user_id=${id}`;
+  window.location.href = `https://c504-80-67-220-252.ngrok-free.app/integrations/${integration}/auth?user_id=${id}`;
 };
