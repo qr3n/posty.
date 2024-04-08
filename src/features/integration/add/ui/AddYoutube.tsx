5addff4fde455eaecb7f652ae19182c7fc80bcf6
qr3n <qren.freelance@gmail.com>
2024-04-08 02:33:34 -0700

    Init commit

--- src/features/integration/add/ui/AddYoutube.tsx
+++ src/features/integration/add/ui/AddYoutube.tsx
@@ -13,7 +13,7 @@ export const AddYoutube = () => {
   return (
     <div className='flex gap-1'>
       <Image src={youtube} alt={'youyube'} width={32} height={8}/>
-      <h1 className='font-semibold text-2xl'>YouTube</h1>
+      <h1 className='font-semibold text-2xl'>Youtube</h1>
     </div>
   );
 };
