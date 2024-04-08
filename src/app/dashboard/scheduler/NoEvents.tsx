730017cfd51ceb0e6746db6bfeae44c53f2a8f6c
qren <qtter85@gmail.com>
2024-04-09 01:14:07 +0700

    Вщьфшт

--- src/app/dashboard/scheduler/NoEvents.tsx
+++ src/app/dashboard/scheduler/NoEvents.tsx
@@ -5,20 +5,22 @@ import { examples } from "./examples/images";
 
 export const NoEvents = () => {
   return (
-    <>
-      <div
-        className='absolute w-full z-40 top-0 left-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 overflow-y-auto px-8 pb-8 pt-8 gap-6'>
-        { examples.map((e, i) => (
-          <AspectRatio key={i} ratio={16 / 9}>
-            <Image
-              className='z-40 w-full h-full object-cover rounded-md'
-              src={e}
-              alt='' width={1920} height={1080}
-            />
-          </AspectRatio>
-        )) }
-      </div>
-      <div className='h-screen w-screen absolute top-0 left-0 bg-gradient-to-t from-40% sm:from-35% from-black to-trasparent z-40'/>
-    </>
+      <>
+          <div
+              className='absolute w-full z-40 top-0 left-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 overflow-y-auto px-8 pb-8 pt-8 gap-6'>
+              {examples.map((e, i) => (
+                  <AspectRatio key={i} ratio={16 / 9}>
+                      <Image
+                          className='z-40 w-full h-full object-cover rounded-md'
+                          src={e}
+                          alt='' width={1920} height={1080}
+                      />
+                  </AspectRatio>
+              ))}
+          </div>
+          <div className='h-screen w-screen absolute top-0 left-0 z-40 bg-black opacity-25'/>
+          <div
+              className='h-screen w-screen absolute top-0 left-0 bg-gradient-to-t from-40% sm:from-45% from-black to-trasparent z-40'/>
+      </>
   )
 }
